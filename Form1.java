e50da296503a8b9eab358294c6daa3719be73a50
ThanhTam1312 <105963849+ThanhTam1312@users.noreply.github.com>
2022-05-21 16:37:24 +0700

    Update Form1.java

--- Form1.java
+++ Form1.java
@@ -1,1 +1,23 @@
+public class Practice1 {
+    public static void main (String[]args){
+        double[][] BangDiemHocSinh =  {{'A', 'B', 'A', 'C', 'D', 'E', 'A', 'D', 'E', 'C'},
+                                       {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+		                                   {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+				                               {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+				                               {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+				                               {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+				                               {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+				                               {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+        double[] Key = 
+            {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+       for(int i = 0 ; i < BangDiemHocSinh.length; i++) {
+			int TinhDiem = 0;
+			for(int j = 0 ; j < BangDiemHocSinh [i] .length ; j++) {
+				if(BangDiemHocSinh[i][j] == Key[j])
+				TinhDiem++;
+        }
+        System.out.println("Diem So cua hoc sinh "  +  i  + " la "  + TinhDiem);
+    }
+    }
+}
 
